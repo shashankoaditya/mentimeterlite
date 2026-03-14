aff56ca96f2949255242fd8976e9df064b41e384
shashankoaditya <134091138+shashankoaditya@users.noreply.github.com>
2026-03-14 15:34:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ def presenter_landing():
 
     st.title("Live Poll")
 
-    participant_url = "https://mentimeterlite-dxem3jgzxnqheyg4ncjcus.streamlit.app/?mode=participant"
+    participant_url = "https://mentimeterlite-dxem3jgznxqheyg4ncjcus.streamlit.app/?mode=participant"
 
     st.write("Scan this QR code to join the poll")
 
